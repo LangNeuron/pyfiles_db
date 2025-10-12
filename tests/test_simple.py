d3b04c0a4915d82e30330e00bccba53925173123
Anton Golubchikov <anton1programmist@gmail.com>
2025-10-12 17:13:58 +0700

    update publish.yml

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Init file for python package."""
+"""Simple test."""
+
+def test_simple() -> None:
+    """Simple test."""
+    return
