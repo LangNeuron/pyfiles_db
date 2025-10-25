2558ff32e7bd7b35fabb0db03ab2b87ac3effbcd
Anton Golubchikov <anton1programmist@gmail.com>
2025-10-26 01:38:40 +0700

    add tests

---
+++ tests/test_creaet_table.py
@@ -0,0 +1,28 @@
+# Copyright 2025 LangNeuron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""test create table."""
+
+from src.pyfiles_db import FilesDB
+
+
+def test_create_table() -> None:
+    file_db = FilesDB()
+    db = file_db.init()
+    db.create_table(table_name="test_craete_table", columns={"id": "INT",
+                                                     "first_name": "TEXT",
+                                                     "last_name": "TEXT",
+                                                     "number": "INT"},
+                         id_generator="id")
---
+++ tests/test_db_find.py
@@ -0,0 +1,35 @@
+# Copyright 2025 LangNeuron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""test database find."""
+
+from src.pyfiles_db import FilesDB
+
+d1 = {"id": 1, "first_name": "John", "last_name": "Doe", "number": 8}
+d2 = {"id": 2, "first_name": "Alex", "last_name": "Wandack", "number": 101}
+db_name = "test_finder"
+
+def test_find() -> None:
+    f = FilesDB()
+    bd = f.init()
+    bd.create_table(db_name, columns={"id": "INT",
+                                            "first_name": "TEXT",
+                                            "last_name": "TEXT",
+                                            "number": "INT"}, id_generator="id")
+    bd.new_data(db_name, d1)
+    bd.new_data(db_name, d2)
+    assert bd.find(db_name, "number == 8") == d1
+    assert bd.find(db_name, "id==1") == d1
+    assert bd.find(db_name, "last_name == Wandack") == d2
