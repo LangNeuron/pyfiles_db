3cb97f52abd13866b5ba50b3ceb1ecc0c9e89c99
Anton Golubchikov <anton1programmist@gmail.com>
2025-10-27 21:00:12 +0700

    fix ruff error

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+"""conftest.py file."""
 
 import shutil
 from collections.abc import Generator
